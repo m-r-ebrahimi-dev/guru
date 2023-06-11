1304853bc66f18bba9d34e09b1d713b2857531da
m-r-ebrahimi-dev <mrebrahimidev@gmail.com>
2023-06-11 18:16:19 +0330

    initialing

---
+++ src/main/java/ir/me/bestguru/BestGuruApplication.java
@@ -0,0 +1,13 @@
+package ir.me.bestguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/me/bestguru/BestGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.me.bestguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
